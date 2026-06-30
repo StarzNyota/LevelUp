94f4e1c72acdd4c954a1370f439fea686101cdd4
StarzNyota <gamestoplayslay@gmail.com>
2026-06-30 04:19:50 -0500

    Rename scrollTo function and add new features

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-function scrollTo(id) {
+function scrollToSection(id) {
   document.getElementById(id).scrollIntoView({
     behavior: "smooth"
   });
+}
+
+// DARK MODE TOGGLE
+function toggleDarkMode() {
+  document.body.classList.toggle("dark");
+}
+
+// CHALLENGE BUTTON
+function acceptChallenge() {
+  alert("Nice 💖 You accepted today’s challenge!");
 }
